c1fe8e24418155044c22a109a8e1d9295e72f979
Nerezea <nerezea@outlook.de>
2023-09-04 11:38:12 +0200

    project page design

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,34 +1,100 @@
 const ProjectPage = () => {
   return (
     <div className="projects">
-      <h2>Simple 3 Columns Layout Flexbox</h2>
-
       <div className="columns-3">
         <div className="column">
           <div className="card">
             <div className="card-side card-side-front">
               <div className="card-picture card-picture-1">&nbsp;</div>
-              <div className="card-heading">Heading</div>
-              <div className="card-details">Details</div>
+              <div className="card-heading">
+                <span className="card-heading-span card-heading-span-1">
+                  Alpaca Farm Project
+                </span>
+              </div>
+              <div className="card-details">
+                <ul>
+                  <li>hhhh</li>
+                  <li>gggg</li>
+                  <li>jjjj</li>
+                  <li>kkkk</li>
+                  <li>llll</li>
+                </ul>
+              </div>
             </div>
             <div className="card-side card-side-back card-side-back-1">
-              BACK
+              <div className="card-side-back-cta">
+                <div className="card-side-back-cta-info">
+                  <p className="card-side-back-cta-info-1">Test</p>
+                  <p className="card-side-back-cta-info-2">Test</p>
+                </div>
+                <button className="card-side-back-cta-button">
+                  To the github
+                </button>
+              </div>
             </div>
           </div>
         </div>
         <div className="column">
           <div className="card">
-            <div className="card-side card-side-front">FRONT</div>
+            <div className="card-side card-side-front">
+              <div className="card-picture card-picture-2">&nbsp;</div>
+              <div className="card-heading">
+                <span className="card-heading-span card-heading-span-2">
+                  Alpaca Farm Project
+                </span>
+              </div>
+              <div className="card-details">
+                <ul>
+                  <li>hhhh</li>
+                  <li>gggg</li>
+                  <li>jjjj</li>
+                  <li>kkkk</li>
+                  <li>llll</li>
+                </ul>
+              </div>
+            </div>
             <div className="card-side card-side-back card-side-back-2">
-              BACK
+              <div className="card-side-back-cta">
+                <div className="card-side-back-cta-info">
+                  <p className="card-side-back-cta-info-1">Test</p>
+                  <p className="card-side-back-cta-info-2">Test</p>
+                </div>
+                <button className="card-side-back-cta-button">
+                  To the github
+                </button>
+              </div>
             </div>
           </div>
         </div>
         <div className="column">
           <div className="card">
-            <div className="card-side card-side-front">FRONT</div>
+            <div className="card-side card-side-front">
+              <div className="card-picture card-picture-3">&nbsp;</div>
+              <div className="card-heading">
+                <span className="card-heading-span card-heading-span-3">
+                  Alpaca Farm Project
+                </span>
+              </div>
+              <div className="card-details">
+                <ul>
+                  <li>hhhh</li>
+                  <li>gggg</li>
+                  <li>jjjj</li>
+                  <li>kkkk</li>
+                  <li>llll</li>
+                </ul>
+              </div>
+            </div>
             <div className="card-side card-side-back card-side-back-3">
-              BACK
+              <div className="card-side-back-cta">
+                <div className="card-side-back-cta-info">
+                  <p className="card-side-back-cta-info-1">Test</p>
+                  <p className="card-side-back-cta-info-2">Test</p>
+                </div>
+                <button className="card-side-back-cta-button">
+                  To the github
+                </button>
+              </div>
             </div>
           </div>
         </div>
