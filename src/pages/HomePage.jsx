ae7e5b7656b8bd302a5c9fd418dcdcd76fc3149e
Nerezea <nerezea@outlook.de>
2023-09-05 11:43:01 +0200

    added utilities and variables

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,34 +1,35 @@
 const HomePage = () => {
   return (
     <main className="homepage">
-      <div className="container">
-        <h1>Welcome to my Portfolio</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-          incidunt architecto reiciendis temporibus sit deserunt delectus quos
-          amet excepturi necessitatibus, odit, similique soluta explicabo quidem
-          nam, perferendis quaerat quis expedita voluptatum at quas dolorem? Ad
-          id voluptatibus fugit nemo dignissimos. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit.
-        </p>
-        <p>
-          Enim explicabo aut porro? Odit porro explicabo nostrum natus. Harum
-          corporis soluta illo maiores quaerat doloremque ipsam temporibus aut.
-          Odio tempora ad recusandae, fugiat neque eum ea! Sapiente nam nisi
-          laboriosam! Asperiores, nulla quia error accusamus tenetur
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-          repellendus, molestias harum eligendi iste tempora, aspernatur eius
-          atque laudantium cum magni reiciendis adipisci! Eligendi id adipisci
-          numquam est quia culpa velit optio dolor? Dolorem porro ipsum
-          similique in inventore sit nam adipisci reprehenderit ab, maxime eaque
-          tempore alias sapiente vero magni eveniet! Pariatur repellendus iusto
-          voluptatibus consectetur eaque temporibus tempore! Doloremque
-          voluptatum quaerat, odio quasi impedit dolor magni tempore. Odit
-          laboriosam aut ex atque optio? Quia expedita hic qui ipsam corrupti,
-        </p>
-      </div>
+      <section className="about-section">
+        <div className="u-center-text u-margin-bottom-big">
+          <h2>Welcome to my Portfolio</h2>
+        </div>
+
+        <div className="about-container">
+          <div className="text-content u-center-text">
+            <h3 className="u-margin-bottom-small">Random Text about me</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus nobis placeat eum illum fugit, nostrum maxime explicabo
+              voluptatibus ipsam tenetur! Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Alias reprehenderit animi suscipit
+              numquam voluptatibus ad rerum, dolorem optio earum culpa!
+            </p>
+            <h3 className="u-margin-bottom-small">
+              Probably even more random text about me
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus nobis placeat eum illum fugit, nostrum maxime explicabo
+              voluptatibus ipsam tenetur! Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Sit aperiam cum esse inventore
+              vitae perspiciatis dolor necessitatibus enim veniam nihil.
+            </p>
+          </div>
+          <div className="image-content">image-content</div>
+        </div>
+      </section>
     </main>
   );
 };
